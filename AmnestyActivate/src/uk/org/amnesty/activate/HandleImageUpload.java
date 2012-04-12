e3780c1a21cd9197626b23e862e87d760ee2a3fc
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-04-12 03:48:21 +0100

    overlaing text info

--- AmnestyActivate/src/uk/org/amnesty/activate/HandleImageUpload.java
+++ AmnestyActivate/src/uk/org/amnesty/activate/HandleImageUpload.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,13 +14,19 @@ import android.widget.Toast;
 
 public class HandleImageUpload  extends Activity{
 
+	private static final String OVERLAY_TEXT1 = "date: 12 Apr 2012";
+	private static final String OVERLAY_TEXT2 = "time 11h:37m:21s GMT";
+	private static final String OVERLAY_TEXT3 = "lon: 51.522047, lat: -0.109654";
 	public static final String FILE_URI = "FILE_URI";
+	private String fileUri;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.handle_image_upload);	
-		String fileUri = getIntent().getStringExtra(FILE_URI);
+		
+		fileUri = getIntent().getStringExtra(FILE_URI);
+		
 		Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();
 		ImageView image = (ImageView) findViewById(R.id.image);
 		
@@ -26,7 +34,8 @@ public class HandleImageUpload  extends Activity{
 		Bitmap overlayedBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.activate_logo);
 		
 		Bitmap result = overlay(fileBitmap, overlayedBitmap);
-		
+		fileBitmap.recycle();
+		overlayedBitmap.recycle();
 		image.setImageBitmap(result);
 	}
 
@@ -35,6 +44,12 @@ public class HandleImageUpload  extends Activity{
         Canvas canvas = new Canvas(bmOverlay);
         canvas.drawBitmap(bmp1, new Matrix(), null);
         canvas.drawBitmap(bmp2, new Matrix(), null);
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setTextSize(100);
+		canvas.drawText(OVERLAY_TEXT1, 50, 250, paint);
+		canvas.drawText(OVERLAY_TEXT2, 50, 400, paint);
+		canvas.drawText(OVERLAY_TEXT3, 50, 550, paint);
         return bmOverlay;
     }
 
