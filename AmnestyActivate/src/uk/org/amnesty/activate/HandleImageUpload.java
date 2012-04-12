48af6e11e43866cd251876be8537c2d993fdac36
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-04-12 04:16:13 +0100

    added status bar

--- AmnestyActivate/src/uk/org/amnesty/activate/HandleImageUpload.java
+++ AmnestyActivate/src/uk/org/amnesty/activate/HandleImageUpload.java
@@ -14,9 +14,10 @@ import android.widget.Toast;
 
 public class HandleImageUpload  extends Activity{
 
-	private static final String OVERLAY_TEXT1 = "date: 12 Apr 2012";
-	private static final String OVERLAY_TEXT2 = "time 11h:37m:21s GMT";
-	private static final String OVERLAY_TEXT3 = "lon: 51.522047, lat: -0.109654";
+	private static final String OVERLAY_TEXT1 = "date: 12 Apr 2012   11:37:21 GMT";
+	private static final String OVERLAY_TEXT2 = "longitude: 51.522047";
+	private static final String OVERLAY_TEXT3 = "latitude: -0.109654";
+	private static final String OVERLAY_TEXT4 = "altitude: 28.38114";
 	public static final String FILE_URI = "FILE_URI";
 	private String fileUri;
 
@@ -46,10 +47,15 @@ public class HandleImageUpload  extends Activity{
         canvas.drawBitmap(bmp2, new Matrix(), null);
         Paint paint = new Paint();
         paint.setColor(Color.YELLOW);
-        paint.setTextSize(100);
-		canvas.drawText(OVERLAY_TEXT1, 50, 250, paint);
-		canvas.drawText(OVERLAY_TEXT2, 50, 400, paint);
-		canvas.drawText(OVERLAY_TEXT3, 50, 550, paint);
+        int textSize = 100;
+		int lineSize = textSize + 50;
+        paint.setTextSize(textSize);
+        int startY = 200;
+        int startX = 50;
+		canvas.drawText(OVERLAY_TEXT1, startX, startY+=lineSize, paint);
+		canvas.drawText(OVERLAY_TEXT2, startX, startY+=lineSize, paint);
+		canvas.drawText(OVERLAY_TEXT3, startX, startY+=lineSize, paint);
+		canvas.drawText(OVERLAY_TEXT4, startX, startY+=lineSize, paint);
         return bmOverlay;
     }
 
