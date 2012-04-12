da6239bd6176d0b0c7d0962f85e292c84bcc6c93
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-04-12 06:41:33 +0100

    looks better on tablet

--- AmnestyActivate/src/uk/org/amnesty/activate/ImageUploadOptions.java
+++ AmnestyActivate/src/uk/org/amnesty/activate/ImageUploadOptions.java
@@ -1,25 +1,16 @@
 package uk.org.amnesty.activate;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class ImageUploadOptions extends Activity{
 	
-	private String fileUri;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.image_upload_options);	
-		
-		fileUri = getIntent().getStringExtra(HandleImageUpload.FILE_URI);
-		
 		ImageView image = (ImageView) findViewById(R.id.image);
-		
 		image.setImageBitmap(HandleImageUpload.result);
 	}
 
